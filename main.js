d67cae3d8b829e33d9d62d9e2f55c033b15e83cf
geekmaros <razicruz@gmail.com>
2020-11-03 23:31:17 +0100

    cleared stage

---
+++ main.js
@@ -0,0 +1,18 @@
+(function($) {
+  "use strict";
+  // decalre actors here
+
+  // clear stage
+
+  //enter floor vegetation
+
+  // enter tree ..stuff
+
+  // enter the greeting text
+
+  // the GO function .... to kick things all off
+  function go() {
+    console.log("...initialing");
+  }
+  go();
+})(jQuery);
