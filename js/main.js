6adc049ff93ece71fdc0b619e91cc0c07af371a4
geekmaros <razicruz@gmail.com>
2020-11-09 12:11:14 +0100

    added enter tree scene

--- js/main.js
+++ js/main.js
@@ -62,6 +62,39 @@
   }
 
   // enter tree
+  function enterTreeStuff(){
+    let treeStuffTl = new TimelineMax();
+    
+    treeStuffTl.staggerFromTo($treeLeaves, 0.5,{scale: 0.2, autoAlpha:0, transformOrigin: 'center bottom'},
+    {scale: 1, autoAlpha:1, transformOrigin: 'center bottom'}, 0.02)
+    .fromTo($nest, 1 , {y:0, scale:0.2, autoAlpha:0, transformOrigin: 'center center'},
+    {y:"-=15", scale:1, autoAlpha:1, transformOrigin: 'center center', ease: Elastic.easeOut}, '+=0.1')
+    .to($nest, 0, {y:'+=15', ease:Bounce.easeOut}, '-=0.2')
+    .add('nest-pop-in')
+    .set($birdHat, {rotation: 12, x:'+=6'})
+    .to($bird, 1.4, {y:'-=39', autoAlpha: 1, ease: Power4.easeInOut}, 'nest-pop-in+=0.1')
+    .add('bird-peeking')
+    .set($birdEyes, {autoAlpha: 0})
+    .set($birdEyes, {autoAlpha: 1}, '+=0.2')
+    .set($birdEyes, {autoAlpha: 0}, '+=0.3')
+    .set($birdEyes, {autoAlpha: 1}, '+=0.2')
+    .to($bird, 0.8, {y: '-=34', ease:Power4.easeInOut})
+    .to($bird, 0.8, {y: '+=8', ease:Power4.easeOut})
+    .to($birdHat, 0.4, {y:'-=12'}, '-=0.6')
+    .to($birdHat, 0.4, {y:0, rotation: 0, x:0, onComplete: startBlinking}, '-=0.2')
+
+    function startBlinking(){
+      let birdBlinksTl = new TimelineMax({repeat: -1, repeatDelay: 5})
+
+      birdBlinksTl
+      .set($birdEyes, {autoAlpha: 0})
+      .set($birdEyes, {autoAlpha: 1}, '+=0.2')
+      .set($birdEyes, {autoAlpha: 0}, '+=1.3')
+      .set($birdEyes, {autoAlpha: 1}, '+=0.2')
+    }
+
+    return treeStuffTl
+  }
 
   // enter the greeting text
 
@@ -75,7 +108,8 @@
     //TODO: add childtimelines to master
     masterTl
       .add(clearStage(), "scene-clear-stage")
-      .add(enterFloorVegetation(), "scene-floor-vegitation");
+      .add(enterFloorVegetation(), "scene-floor-vegitation")
+      .add(enterTreeStuff(), "tree-stuff");
   }
   start();
 })(jQuery);
